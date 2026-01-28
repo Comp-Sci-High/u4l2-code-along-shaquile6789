7fe4b5679211acd25d02360da99cfdf1d5008c45
shaquile6789 <shaquille.oemrawsingh26@compscihigh.org>
2026-01-28 17:44:19 +0000

    done

--- index.js
+++ index.js
@@ -14,18 +14,23 @@ app.use((req, res, next) => {
 
 // Step 3: Serve Static Files w/ middleware
 // Remeber to use the variable __dirname
-
+app.use(express.static(__dirname + '/public'))
 
 
 
 // Step 4: Create a route handler for the home route
 // Once inside send the file index.html
 
-
+app.get('/bulba', (req,res) => {
+  res.sendFile(__dirname + '/public/index.html')
+})
 
 // OYO: Create a route handler for /js that sends only the client side javaScript
 // run your server and test your code
 
+app.get('/js', (req,res) => {
+  res.sendFile(__dirname + '/public/bulbasaur.js')
+})
 
 app.listen(3000, () => {
   console.log("Server is serving")
